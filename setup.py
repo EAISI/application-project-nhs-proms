911ba320cad3eecab7d306d66033f2904b442b40
Daniel Kapitan <dkapitan>
2020-03-08 19:45:10 +0100

    initial commit cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data science case study using the NHS Digital PROMs data of hip and knee replacements.',
+    author='Daniel Kapitan',
+    license='MIT',
+)
